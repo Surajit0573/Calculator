f2fec1ade40953d78b2abfe8ba24581300542e55
sourabh <sourabhsharma4779@gmail.com>
2023-10-10 02:53:06 +0530

    Calculator new buttons.

--- calculator.js
+++ calculator.js
@@ -19,3 +19,25 @@ function calculateResult() {
         document.getElementById('display').value = displayValue;
     }
 }
+
+
+function calculateModulo() {
+    try {
+        displayValue = eval(displayValue) % 2;
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
+
+
+function calculateRoot() {
+    try {
+        displayValue = Math.sqrt(eval(displayValue)); 
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
