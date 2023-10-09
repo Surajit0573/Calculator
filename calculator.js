fb746df7e7fee32aed68eed17e59f11bb07895c5
Gurjot Singh <gurjotsingh03@outlook.com>
2023-10-09 23:13:18 +0530

    added functions

--- calculator.js
+++ calculator.js
@@ -19,3 +19,36 @@ function calculateResult() {
         document.getElementById('display').value = displayValue;
     }
 }
+function calculateLog() {
+    try {
+        const inputValue = parseFloat(displayValue);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            displayValue = 'log(' + inputValue + ')';
+            document.getElementById('display').value = displayValue;
+            displayValue = Math.log(inputValue);
+        } else {
+            displayValue = 'Error: Invalid Input';
+            document.getElementById('display').value = displayValue;
+        }
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
+
+function calculateSquareRoot() {
+    try {
+        const inputValue = parseFloat(displayValue);
+        if (!isNaN(inputValue) && inputValue >= 0) {
+            displayValue = '√(' + inputValue + ')';
+            document.getElementById('display').value = displayValue;
+            displayValue = Math.sqrt(inputValue);
+        } else {
+            displayValue = 'Error: Invalid Input';
+            document.getElementById('display').value = displayValue;
+        }
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
