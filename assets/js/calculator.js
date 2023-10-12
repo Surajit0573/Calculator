80ae3652cde3d98ea9e97d02fe3dc0e61af98990
Purna Shrestha <crush9918@gmail.com>
2023-10-12 22:33:03 +0545

    Create a percentage-feature plus better file storage

---
+++ assets/js/calculator.js
@@ -0,0 +1,34 @@
+let displayValue = "";
+let ans = "0";
+
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue);
+    document.getElementById("display").value = displayValue;
+    ans = displayValue;
+  } catch (error) {
+    displayValue = "Error";
+    document.getElementById("display").value = displayValue;
+  }
+}
+
+function previousValue() {
+  displayValue += ans.toString();
+  document.getElementById("display").value = displayValue;
+}
+
+// Calculate the percentage of the current display value
+function calculatePercentage() {
+  displayValue = (parseFloat(displayValue) / 100).toString();
+  document.getElementById("display").value = displayValue;
+}
